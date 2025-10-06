95560f9cd50bbc05756687133e085f101aa47f02
FiorellaDiGiulio <fiorella.digiulio@chasacademy.se>
2025-10-06 10:03:09 +0200

    uppdatera

---
+++ script.js
@@ -0,0 +1,6 @@
+//  Hamburger-meny från gamla filen 
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
